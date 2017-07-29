66d46daea9942a23540bb4669c47aaa06ac77de4
Lalit Adithya <lalitadithya5071@gmail.com>
2017-07-29 12:29:51 +0530

    Added model for reserves

---
+++ SailorsBoats/Models/Reserves.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailorsBoats.Models
+{
+    class Reserves
+    {
+        public int SailorId { get; set; }
+        public int BoatId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
