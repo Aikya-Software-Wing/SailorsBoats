542ceb7c5894b607e6518b1d8df105b01f8b1fa7
Lalit Adithya <lalitadithya5071@gmail.com>
2017-07-29 13:00:52 +0530

    Added validator for reserve

---
+++ SailorsBoats/Validators/ReserveValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailorsBoats.Validators
+{
+    class ReserveValidator
+    {
+        public static bool IsSailorIdValid(string sailorId, out string errorMessage)
+        {
+            errorMessage = "";
+            return true;
+        }
+
+        public static bool IsBoatIdValid(string boatId, out string errorMessage)
+        {
+            errorMessage = "";
+            return true;
+        }
+
+        public static bool IsDateValid(string date, out string errorMessage)
+        {
+            errorMessage = "";
+            return true;
+        }
+    }
+}
