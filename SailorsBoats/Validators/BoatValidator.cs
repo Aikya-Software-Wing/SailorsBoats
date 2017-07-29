5922759a2041b21a0c7bef49265d9abf982bc4bf
Lalit Adithya <lalitadithya5071@gmail.com>
2017-07-29 12:59:12 +0530

    Added validator for boat

---
+++ SailorsBoats/Validators/BoatValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailorsBoats.Validators
+{
+    class BoatValidator
+    {
+        public static bool IsIdValid(string id, out string errorMessage)
+        {
+            errorMessage = "";
+            return true;
+        }
+
+        public static bool IsNameValid(string name, out string errorMessage)
+        {
+            errorMessage = "";
+            return true;
+        }
+
+        public static bool IsColorValid(string rating, out string errorMessage)
+        {
+            errorMessage = "";
+            return true;
+        }
+    }
+}
