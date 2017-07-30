1dd4628847cd6ab73ce0be6b25a7229c421c9223
Lalit Adithya <lalitadithya5071@gmail.com>
2017-07-30 20:50:13 +0530

    Added validation for sailor

---
+++ SailorsBoats/Util/IntegerValidator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailorsBoats.Util
+{
+    class IntegerValidator
+    {
+        public static bool IsIntegerValid(string integer, string propertyName, out string errorMessage)
+        {
+            int _integer;
+
+            if (!int.TryParse(integer, out _integer))
+            {
+                errorMessage = propertyName + " must be an integer";
+                return false;
+            }
+            else
+            {
+                errorMessage = "";
+                return true;
+            }
+        }
+
+        public static bool IsIntegerValidAndPostive(string integer, string propertyName,
+            out string errorMessage)
+        {
+            int _integer;
+
+            if (!int.TryParse(integer, out _integer))
+            {
+                errorMessage = propertyName + " must be an integer";
+                return false;
+            }
+            else
+            {
+                if (_integer < 0)
+                {
+                    errorMessage = propertyName + " must be positive";
+                    return false;
+                }
+                else
+                {
+                    errorMessage = "";
+                    return true;
+                }
+            }
+        }
+    }
+}
---
+++ SailorsBoats/Util/StringValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailorsBoats.Util
+{
+    class StringValidator
+    {
+        public static bool IsStringNotNullOrEmpty(string _string, string propertyName,
+            out string errorMessage)
+        {
+            if (string.IsNullOrWhiteSpace(_string))
+            {
+                errorMessage = propertyName + " can not be empty";
+                return false;
+            }
+            else
+            {
+                errorMessage = "";
+                return true;
+            }
+        }
+    }
+}
