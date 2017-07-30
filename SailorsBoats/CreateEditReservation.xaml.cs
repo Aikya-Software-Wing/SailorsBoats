698f8a6b48d07e3e3e5e9d9ed67337421ac52deb
Lalit Adithya <lalitadithya5071@gmail.com>
2017-07-30 21:04:37 +0530

    Removed dead code

--- SailorsBoats/CreateEditReservation.xaml.cs
+++ SailorsBoats/CreateEditReservation.xaml.cs
@@ -49,12 +49,6 @@ namespace SailorsBoats
 
         private void Save_Button_Click(object sender, RoutedEventArgs e)
         {
-            if (!ValidateReservationAndDisplayMessages(SailorName_TextBox.Text, BoatName_TextBox.Text, 
-                ReservationDate_TextBox.Text))
-            {
-                return;
-            }
-
             Reserve reserve = GetReserveObjectFromInput();
 
             if (ReservationId == -1)
@@ -103,30 +97,6 @@ namespace SailorsBoats
             return reserve;
         }
 
-        private bool ValidateReservationAndDisplayMessages(string sailorId, string boatId, string date)
-        {
-            bool allPropertiesValid = true;
-
-            if (!ReserveValidator.IsSailorIdValid(sailorId, out string errorMessage))
-            {
-                allPropertiesValid = false;
-                SailorName_ValidationLabel.Content = errorMessage;
-            }
-
-            if (!ReserveValidator.IsBoatIdValid(boatId, out errorMessage))
-            {
-                allPropertiesValid = false;
-                BoatName_ValidationLabel.Content = errorMessage;
-            }
-
-            if (!ReserveValidator.IsDateValid(date, out errorMessage))
-            {
-                allPropertiesValid = false;
-                ReservationDate_ValidationLabel.Content = errorMessage;
-            }
-
-            return allPropertiesValid;
-        }
         #endregion
     }
 }
