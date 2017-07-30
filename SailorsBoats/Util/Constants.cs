bbec2abd38cdb8a321f184bfb50a0f04a9f26baf
Lalit Adithya <lalitadithya5071@gmail.com>
2017-07-30 17:10:13 +0530

    Added connection string to app.config. Added class with all application constants

---
+++ SailorsBoats/Util/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SailorsBoats.Util
+{
+    public class Constants
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["SailorBoatsDatabase"].ConnectionString;
+            }
+        }
+    }
+}
